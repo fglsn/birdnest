5dcc92cb5189f740fcd72c861cebc8df1997d742
Ilona Shakurova <leverseau19@gmail.com>
2023-01-14 20:00:06 +0200

    Add foreign key and delete on cascade

--- server/migrations/sqls/20230112092607-initial-migration-up.sql
+++ server/migrations/sqls/20230112092607-initial-migration-up.sql
@@ -1,12 +1,3 @@
-create table drone_position_entries (
-	id bigserial primary key,
-	serial_number varchar not null,
-	position_x double precision not null,
-	position_y double precision not null,
-	distance double precision not null,
-	captured_at timestamptz not null
-);
-
 create table pilots (
 	pilot_name varchar not null,
 	phone_number varchar not null,
@@ -14,6 +5,15 @@ create table pilots (
 	serial_number varchar not null unique,
 	last_seen timestamptz not null,
 	closest_distance double precision not null
+);
+
+create table drone_positions (
+	id bigserial primary key,
+	serial_number varchar not null references pilots (serial_number) on delete cascade,
+	position_x double precision not null,
+	position_y double precision not null,
+	distance double precision not null,
+	captured_at timestamptz not null
 );
 
 create index pilots_serial_number on pilots (serial_number);
