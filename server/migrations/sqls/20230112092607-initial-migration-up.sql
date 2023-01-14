66c6fb765862e6a733a9cc6e43ac140f8d9a4259
Ilona Shakurova <leverseau19@gmail.com>
2023-01-14 18:38:53 +0200

    Create table to store pilots info with last seen and closest_distance

--- server/migrations/sqls/20230112092607-initial-migration-up.sql
+++ server/migrations/sqls/20230112092607-initial-migration-up.sql
@@ -1,11 +1,19 @@
-create table violator_entries (
+create table drone_position_entries (
 	id bigserial primary key,
+	serial_number varchar not null,
+	position_x double precision not null,
+	position_y double precision not null,
+	distance double precision not null,
+	captured_at timestamptz not null
+);
+
+create table pilots (
 	pilot_name varchar not null,
 	phone_number varchar not null,
 	email varchar not null,
 	serial_number varchar not null unique,
-	position_x double precision not null,
-	position_y double precision not null,
-	distance double precision not null,
-	last_seen timestamptz not null
-)
+	last_seen timestamptz not null,
+	closest_distance double precision not null
+);
+
+create index pilots_serial_number on pilots (serial_number);
