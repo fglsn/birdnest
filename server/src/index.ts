b15e1881ec8402843f2659ae981ba2ab2354118e
Ilona Shakurova <leverseau19@gmail.com>
2023-01-15 22:00:10 +0200

    Add recurring function to update pilot state in db

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,16 @@
-import { app } from "./app";
+import { app, updatePilotsState } from './app';
+import { setIntervalAsync } from 'set-interval-async';
+import { clearExpiredEntries } from './queries';
 
 const PORT = 3001;
+
+setIntervalAsync(async () => {
+	console.log(new Date().toUTCString(), 'Fetching report, updating pilots state.');
+	await updatePilotsState();
+
+	console.log('Clearing expired pilot entries.');
+	await clearExpiredEntries();
+}, 2000);
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
