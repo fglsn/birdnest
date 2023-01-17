d7474e3b35b3db5b94756dec0208669250605d3d
Ilona Shakurova <leverseau19@gmail.com>
2023-01-17 10:51:20 +0200

    Add err catching for background process

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,15 @@ import { clearExpiredEntries } from './queries';
 const PORT = 3001;
 
 setIntervalAsync(async () => {
-	console.log(new Date().toUTCString(), 'Fetching report, updating pilots state.');
-	await updatePilotsState();
+	try {
+		console.log(new Date().toUTCString(), 'Fetching report, updating pilots state.');
+		await updatePilotsState();
 
-	console.log('Clearing expired pilot entries.');
-	await clearExpiredEntries();
+		console.log('Clearing expired pilot entries.');
+		await clearExpiredEntries();
+	} catch (err) {
+		console.log('Error occured in the backgroud: ', err);
+	}
 }, 2000);
 
 app.listen(PORT, () => {
